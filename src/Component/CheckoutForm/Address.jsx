de939be504265826100454c9408b9261c8da5f52
jim-2000 <jimthecaptaionofsill@gmail.com>
2021-03-25 06:03:36 +0600

    fethching subdivitions one by one country

--- src/Component/CheckoutForm/Address.jsx
+++ src/Component/CheckoutForm/Address.jsx
@@ -13,18 +13,29 @@ const [shippingSubDivition, setshippingSubDivition] = useState([])
 const [shippingOptions, setshippingOption] = useState('')
 const [Options, setOptions] = useState([])
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+const countries =  Object.entries(shippingCountry).map(([code, name])=>({id:code, label: name}) )
+const subdivisons =  Object.entries(shippingSubDivition).map(([code, name])=>({id:code, label: name}) )
+
 const fetchShippingCountries = async (checkoutTokenId) => {
     const { countries  } = await commerce.services.localeListCountries(checkoutTokenId);
-
-    console.log('country list', countries )
     setShippingCountry(countries)
     setCountry(Object.keys(countries)[0]);
-    console.log('countyr code 01',Object.keys(countries)[0] )
 }
+const fetchSubDivition = async (countryCode) => {
+    const { subdivisions  } = await commerce.services.localeListSubdivisions(countryCode)
+    setshippingSubDivition(subdivisions )
+    setSubDivition(Object.keys(subdivisions )[0]);
+}
+
 
 useEffect(()=> {
     fetchShippingCountries(checkoutToken.id)
 },[])
+
+useEffect(()=> {
+    if(Country) fetchSubDivition(Country)
+},[Country])
+
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     return (
@@ -68,26 +79,34 @@ useEffect(()=> {
                         />
                         <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Country</InputLabel>
-                            <Select value={shippingCountry} fullWidth onChange={(e)=> setShippingCountry(e.target.value)}>
-                                {
-                                   console.log( Object.entries(shippingCountry))
-                                }
+                            <Select value={Country} fullWidth onChange={(e)=> setCountry(e.target.value)}>
+                               {
+                                   countries.map((item)=>(
+                                    <MenuItem key={item.id} value={item.id}>
+                                    {item.label}
+                                
+                                </MenuItem>
+                                   ))
+                               }
 
-                                {/* <MenuItem key={} value={}>
-                                    Select Me
-                                </MenuItem> */}
+                               
+                            </Select>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping SubDivition</InputLabel>
+                            <Select value={SubDivition} fullWidth onChange={(e)=> setSubDivition(e.target.value)}>
+                            {
+                                   subdivisons.map((item)=>(
+                                    <MenuItem key={item.id} value={item.id}>
+                                    {item.label}
+                                
+                                </MenuItem>
+                                   ))
+                               }
                             </Select>
                         </Grid>
  {/*
-                        <Grid item xs={12} sm={6}>
-                            <InputLabel>Shipping SubDivition</InputLabel>
-                            <Select value={} fullWidth onChange>
-                                <MenuItem key={} value={}>
-                                    Select Me
-                                </MenuItem>
-                            </Select>
-                        </Grid>
-
                         <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Options</InputLabel>
                             <Select value={} fullWidth onChange>
